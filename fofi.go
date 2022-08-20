d8abe6482825c36acc32a43d5c70826e85e9ab79
James Chung <james@chung.contact>
2022-08-20 17:47:48 -0500

    update fofi

--- fofi.go
+++ fofi.go
@@ -4,16 +4,24 @@ import (
 	"context"
 )
 
-// Broadcast
+// Broadcast takes an 'in' channel of T and will broadcast that value
+// to every 'out' channel of T.
+//
+// It returns a cancel function.
 func Broadcast[T any](ctx context.Context, in <-chan T, out ...chan T) context.CancelFunc {
 	ctx, cancel := context.WithCancel(ctx)
 	go func() {
+		defer func() {
+			for _, c := range out {
+				close(c)
+			}
+		}()
 		for {
-			val := <-in
 			select {
 			case <-ctx.Done():
 				return
 			default:
+				val := <-in
 				for _, ch := range out {
 					ch <- val
 				}
@@ -23,23 +31,41 @@ func Broadcast[T any](ctx context.Context, in <-chan T, out ...chan T) context.C
 	return cancel
 }
 
-// Coalesce
+// GenerateBroadcasters takes an 'in' channel of T and will generate 'n' number of
+// channels.
+//
+// It returns a slice of type T channels and a CancelFunc.
+func GenerateBroadcasters[T any](ctx context.Context, in <-chan T, n int) ([]chan T, context.CancelFunc) {
+	chs := make([]chan T, n)
+	for i := 0; i < n; i++ {
+		chs[i] = make(chan T)
+	}
+	cancel := Broadcast(ctx, in, chs...)
+	return chs, cancel
+}
+
+// Coalesce will create a channel of T with n 'bufferSize' and a slice of m channels
+// of T.
+//
+// It returns a receive channel of T and a CancelFunc.
 func Coalesce[T any](ctx context.Context, bufferSize int, in ...chan T) (<-chan T, context.CancelFunc) {
 	ch := make(chan T, bufferSize)
 	ctx, cancel := context.WithCancel(ctx)
-	go func() {
-		for _, c := range in {
-			go func(c <-chan T) {
-				for {
-					select {
-					case <-ctx.Done():
+	for _, c := range in {
+		go func(c <-chan T) {
+			for {
+				select {
+				case <-ctx.Done():
+					return
+				default:
+					if v, ok := <-c; ok {
+						ch <- v
+					} else {
 						return
-					default:
-						ch <- <-c
 					}
 				}
-			}(c)
-		}
-	}()
+			}
+		}(c)
+	}
 	return ch, cancel
 }
