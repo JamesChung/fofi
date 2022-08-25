af35047a9c962153ad07935e4d621372151c8695
James Chung <james@chung.contact>
2022-08-25 17:06:35 -0500

    v0.4.1
    - fix: allow nonblocking writes to output channels and allow recovery from edge cases

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [0.4.1] - 2022-08-25
+- Fix: allow blocking writes to output channels and allow recovery from edge case panics.
+
 ## [0.4.0] - 2022-08-24
 ### Changed
 - Minor change to the underlying `select` statement in `Coalesce` and `Broadcast` to make it more idiomatic.
--- fofi.go
+++ fofi.go
@@ -2,6 +2,7 @@ package fofi
 
 import (
 	"context"
+	"log"
 	"sync"
 )
 
@@ -27,8 +28,21 @@ func Broadcast[T any](ctx context.Context, in <-chan T, out ...chan T) context.C
 			case <-ctx.Done():
 				return
 			case val := <-in:
-				for _, ch := range out {
-					ch <- val
+				for i, ch := range out {
+					// For every output channel we create a goroutine which
+					// will handle writing the 'in' value out to each channel
+					// without blocking parent goroutine.
+					go func(i int, ch chan<- T) {
+						// Given a scenario where this transient goroutine is blocked
+						// long past when someone cancels, make sure to recover from
+						// writing to a closed channel and log output.
+						defer func() {
+							if recover() != nil {
+								log.Printf("fofi: broadcast failed to send value %v on closed channel #%d", val, i)
+							}
+						}()
+						ch <- val
+					}(i, ch)
 				}
 			}
 		}
